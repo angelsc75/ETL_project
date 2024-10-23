12247424601c99e6f7e0a77a3ec6e63ef5fbcc1b
angelsc75 <asc7es@gmail.com>
2024-10-23 12:14:49 +0200

    cambio para hacer llamadas a las variables de entorno.
    ojo: en las variables de entorno teneis que incluir: KAFKA_GROUP_ID=my-group en mi caso se llama my-group, en el de maría rosa era hr_group...

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ main.load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 if __name__ == "__main__":
     # Configuraciones 
     kafka_broker = os.getenv('KAFKA_BROKER')
+    kafka_group= os.getenv('KAFKA_GROUP_ID')
     mongo_uri = os.getenv('MONGO_URI')
     db_name= os.getenv('DB_NAME')
     collection_name= os.getenv('COLLECTION_NAME')
@@ -21,13 +22,13 @@ if __name__ == "__main__":
     db_pass = os.getenv('DB_PASSWORD')
 
     # Inicializa el loader de MongoDB
-    mongo_loader = MongoDBLoader(uri=mongo_uri, db_name="hr_data", collection_name="data")
+    mongo_loader = MongoDBLoader(uri=mongo_uri, db_name=db_name, collection_name=collection_name)
     # Inicializa el loader de MongoDB
     sql_loader = SQLloader(host=db_host, database=db_name, 
     user=db_user, password=db_pass, port=db_port)
 
     # Inicializa el consumidor de Kafka
-    kafka_consumer = KafkaConsumer(kafka_broker, "hrpro-group", mongo_loader, sql_loader)
+    kafka_consumer = KafkaConsumer(kafka_broker, kafka_group, mongo_loader, sql_loader)
 
     # Comienza a consumir mensajes de Kafka y cargarlos en las bases de datos
     kafka_consumer.start_consuming()
