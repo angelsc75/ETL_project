6b84346b32dff008cab41195473db90d28b36b20
angelsc75 <asc7es@gmail.com>
2024-10-24 12:47:51 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,38 +7,9 @@ from dotenv import main
 
 main.load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
-
-
-
-
-
-
 if __name__ == "__main__":
     # Configuraciones Kafka
     kafka_broker = os.getenv('KAFKA_BROKER')
-<<<<<<< Updated upstream
-    mongo_uri = os.getenv('MONGO_URI')
-    db_name= os.getenv('DB_NAME')
-    collection_name= os.getenv('COLLECTION_NAME')
-
-
-    db_type = os.getenv('DB_TYPE')
-    db_host = os.getenv('DB_HOST')
-    db_port = os.getenv('DB_PORT')
-    db_name = os.getenv('DB_DB')
-    db_schema = os.getenv('DB_SCHEMA')
-    db_user = os.getenv('DB_USER')
-    db_pass = os.getenv('DB_PASSWORD')
-
-    # Inicializa el loader de MongoDB
-    mongo_loader = MongoDBLoader(uri=mongo_uri, db_name="hr_data", collection_name="data")
-    # Inicializa el loader de MongoDB
-    sql_loader = SQLloader(host=db_host, database=db_name, 
-    user=db_user, password=db_pass, port=db_port)
-
-    # Inicializa el consumidor de Kafka
-    kafka_consumer = KafkaConsumer(kafka_broker, "hrpro-group", mongo_loader, sql_loader)
-=======
     kafka_group = os.getenv('KAFKA_GROUP_ID')
     
     # Configuración Redis
@@ -90,7 +61,6 @@ if __name__ == "__main__":
         mongo_loader,
         sql_loader
     )
->>>>>>> Stashed changes
 
     # Comenzar a consumir mensajes
     kafka_consumer.start_consuming()
